3472c34b1bc111de2d90b1de11482b3ce272e3f9
JakubMakarewicz <jakub.makarewicz@>
2023-05-01 13:37:30 +0200

    WiSP-LAB_4: ZAD_1

---
+++ WiSP/LAB_4/ZAD_1.py
@@ -0,0 +1,34 @@
+from OpenGL.GLUT import *
+from OpenGL.GL import *
+import numpy as np
+
+n = int(input("Please provide n: "))
+a = float(input("Please provide a: "))
+
+
+def show():
+	angleIncrement = 360. / n
+	angleIncrement *= np.pi / 180.
+	glBegin(GL_TRIANGLE_FAN)
+
+	angle = 0.
+
+	#(s / 2) / r = sin(a / 2)
+	#s / 2 = r * sin(a / 2)
+	r = a / (2 * np.sin(a / 2))
+
+	for _ in range(n):
+		glVertex3f(r * np.cos(angle), r * np.sin(angle), 0.)
+		angle += angleIncrement
+	glEnd()
+	glutSwapBuffers()
+
+glutInit()
+glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE)
+glutInitWindowSize(800, 800)
+glutInitWindowPosition(100, 100)
+glutCreateWindow("Program 02")
+glClearColor(1.0, 1.0, 1.0, 1.0)
+glutDisplayFunc(show)
+glutMainLoop()
+
